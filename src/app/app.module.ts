bb6e08727a690e325bca2169d25c31fff746744f
Saglai <power1800w@gmail.com>
2023-10-19 23:02:44 +0700

    feat: add parent, child, other components, add templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './pages/parent/parent.component';
+import { ChildComponent } from './pages/child/child.component';
+import { MainButtonComponent } from './components/main-button/main-button.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    MainButtonComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
