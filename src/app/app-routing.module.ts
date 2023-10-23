ee109b81b311330a261d6262dfa869792d2b7b8c
Saglai <power1800w@gmail.com>
2023-10-23 19:56:39 +0700

    docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TemplatesPagesComponent } from './pages/templates-pages/templates-pages.component';
-import { ParentComponent } from './pages/viewChildContenChild/parent/parent.component';
-import { AnotherParentComponent } from './pages/life-cycle-hooks/another-parent/another-parent.component';
-import { VEHomeComponent } from './pages/view-encapsulation/v-e-home/v-e-home.component';
-import { DataBindingComponent } from './pages/data-binding/data-binding/data-binding.component';
+import { TemplatesPagesComponent } from './pages/block-one/templates-pages/templates-pages.component';
+import { ParentComponent } from './pages/block-one/viewChildContenChild/parent/parent.component';
+import { AnotherParentComponent } from './pages/block-one/life-cycle-hooks/another-parent/another-parent.component';
+import { VEHomeComponent } from './pages/block-one/view-encapsulation/v-e-home/v-e-home.component';
+import { DataBindingComponent } from './pages/block-one/data-binding/data-binding.component';
 
 const routes: Routes = [
   {path: 'templates', component: TemplatesPagesComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentComponent } from './pages/viewChildContenChild/parent/parent.component';
-import { ChildComponent } from './pages/viewChildContenChild/child/child.component';
+import { ParentComponent } from './pages/block-one/viewChildContenChild/parent/parent.component';
+import { ChildComponent } from './pages/block-one/viewChildContenChild/child/child.component';
 import { MainButtonComponent } from './components/main-button/main-button.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
-import { TemplatesPagesComponent } from './pages/templates-pages/templates-pages.component';
-import { AnotherParentComponent } from './pages/life-cycle-hooks/another-parent/another-parent.component';
-import { AnotherChildComponent } from './pages/life-cycle-hooks/another-child/another-child.component';
+import { TemplatesPagesComponent } from './pages/block-one/templates-pages/templates-pages.component';
+import { AnotherParentComponent } from './pages/block-one/life-cycle-hooks/another-parent/another-parent.component';
+import { AnotherChildComponent } from './pages/block-one/life-cycle-hooks/another-child/another-child.component';
 import { FormsModule } from '@angular/forms';
-import { VENoneComponent } from './pages/view-encapsulation/v-e-none/v-e-none.component';
-import { VEEmulatedComponent } from './pages/view-encapsulation/v-e-emulated/v-e-emulated.component';
-import { VEShadowComponent } from './pages/view-encapsulation/v-e-shadow/v-e-shadow.component';
-import { VEShadowChildComponent } from './pages/view-encapsulation/v-e-shadow-child/v-e-shadow-child.component';
-import { VEHomeComponent } from './pages/view-encapsulation/v-e-home/v-e-home.component';
-import { DataBindingComponent } from './pages/data-binding/data-binding/data-binding.component';
+import { VENoneComponent } from './pages/block-one/view-encapsulation/v-e-none/v-e-none.component';
+import { VEEmulatedComponent } from './pages/block-one/view-encapsulation/v-e-emulated/v-e-emulated.component';
+import { VEShadowComponent } from './pages/block-one/view-encapsulation/v-e-shadow/v-e-shadow.component';
+import { VEShadowChildComponent } from './pages/block-one/view-encapsulation/v-e-shadow-child/v-e-shadow-child.component';
+import { VEHomeComponent } from './pages/block-one/view-encapsulation/v-e-home/v-e-home.component';
+import { DataBindingComponent } from './pages/block-one/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
